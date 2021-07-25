52ccbb2d8a41ae18026fe4cdad176a0a662f2666
Muhammad Arshad <asim.mughal61@gmail.com>
2021-07-25 00:17:04 -0500

    first commit adding spring-security-jpa project

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello admin";
+    }
+}
